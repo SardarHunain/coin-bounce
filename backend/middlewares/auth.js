865537d0ea1a8c8fb61b64018d7a6630d6994c3f
Hunain Pervaiz <hunainpervaiz487@gmail.com>
2023-07-09 00:25:39 +0500

    some changes

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,36 +1,44 @@
 const JWTService = require('../services/JWTService');
 const User = require('../models/user');
-const UserDTO = require('../dtos/user');
+const UserDTO = require('../dto/user');
 
 const auth = async(req,res,next)=>{
     //refresh, access token validation
-    const {refreshToken,accessToken} = req.cookies;
+    try{
+        const {refreshToken,accessToken} = req.cookies;
 
-    if(!refreshToken||!accessToken)
-    {
-        const error = {
-            status: 401,
-            message: 'unauthorized'
+        if(!refreshToken||!accessToken)
+        {
+            const error = {
+                status: 401,
+                message: 'unauthorized'
+            }
+            return next(error);
         }
-        return next(error);
-    }
-    let_id;
-    try{
-        _id = JWTService.verifyAccessToken(accessToken);
-    }
-    catch(error){
-        return next(error);
-    }
-
-    let user;
-    try{
-        user = await User.findOne({_id: _id});
-    }
-    catch(error){
-        return next(error);
-    }
-    const userDto = new UserDto(user);
-    req.user = userDto;
+        let _id;
+        try{
+            _id = JWTService.verifyAccessToken(accessToken);
+        }
+        catch(error){
+            return next(error);
+        }
     
-    next();
+        let user;
+        try{
+            user = await User.findOne({_id: _id});
+        }
+        catch(error){
+            return next(error);
+        }
+        const userDto = new UserDTO(user);
+        req.user = userDto;
+        
+        next();
+    
+    }
+    catch(error){
+        return next(error);
+    }
+    
 }
+module.exports = auth;
